b3e0d6b8f2939ed2e1acd399de5b0df0fc87b62a
harper <jhjin2u@gmail.com>
2023-03-06 17:55:24 +0900

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Url from './Url';
+
+function App() {
+  return (
+    <div className='App'>
+      <Url />
+      <Url />
+    </div>
+  );
+}
+export default App;
---
+++ src/Url.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Url = () => {
+    return(
+        <div calssName = "Url">
+            Url 컴포넌트 만들기
+        </div>
+    )
+}
+export default Url;

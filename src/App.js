4c744319382452a4d3d68b5fa2d50fe0b7ea1f67
harper <jhjin2u@gmail.com>
2023-03-07 00:40:45 +0900

    feat: add url

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import './App.css';
 import Url from './Url';
-import React, {useState} from "react"
+import React, { useState } from "react";
+import { Container, List, Paper } from "@mui/material";
+import AddUrl from './AddUrl';
 
 function App() {
   const [items, setItems] = useState([
     {
       id: "0",
-      title: "Hello.com"
+      destination: "Hello.com",
+      newUrl: "",
+      active: true
     },
     {
       id: "1",
-      title: "World.com"
+      destination: "World.com",
+      newUrl: "",
+      active: true
     },
   ]);
 
-  let UrlItems = items.length > 0 && items.map((item) => <Url item={item} key={item.id} />);
+  const addItem = (item) => {
+    item.id = "" + items.length;
+    item.active = false;
+    //레퍼런스를 기준으로 랜더링 되기 때문에 업데이트는 setItems에서 새 배열 추가로 
+    setItems([...items, item]);
+    console.log("items: ", items)
+  };
+
+  let UrlItems = items.length > 0 && (
+    <Paper stype={{margin: 16}}>
+      <List>
+        {items.map((item) => (
+          <Url item={item} key={item.id} />
+        ))}
+      </List>
+    </Paper>
+  );
   // 초기화 한 item props 사용해 매개변수로 넘기기 
   return (
     <div className='App'>
-      {UrlItems}
+      <Container maxWidth="md">
+        <AddUrl addItem = {addItem} />
+        <div className='UrlList'>{UrlItems}</div>
+      </Container>
     </div>
   );
 }
--- src/Url.js
+++ src/Url.js
@@ -1,11 +1,23 @@
 import React, {useState} from "react"
+import { ListItem, ListItemText, InputBase } from "@mui/material";
 
 const Url = (props) => {
     const [item, setItem] = useState(props.item);
     return(
-        <div calssName = "Url">
-            <p id = "{item.id}">{item.title}</p>
-        </div>
+        <ListItem>
+            <ListItemText>
+                <InputBase
+                    inputProps={{"aria-label": "naked"}}
+                    type="text"
+                    id = {item.id}
+                    name = {item.id}
+                    value = {item.title}
+                    multiline = {true}
+                    fullWidth = {true}
+                />
+            </ListItemText>
+        </ListItem>
     )
+
 }
 export default Url;
